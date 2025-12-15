8b1e3d00d6519d662c24dd6b5a92a4004ba2c8f5
Roman <googlik999@yandex.ru>
2025-12-15 16:21:30 +0300

    test

--- infra/gateway-server/src/main/java/ru/practicum/GatewayServer.java
+++ infra/gateway-server/src/main/java/ru/practicum/GatewayServer.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class Gateway {
+public class GatewayServer {
     public static void main(String[] args) {
-        SpringApplication.run(Gateway.class, args);
+        SpringApplication.run(GatewayServer.class, args);
     }
 }
