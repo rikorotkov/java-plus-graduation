12d902b98ff58e0adb487154cd837d3b8745463c
Roman <googlik999@yandex.ru>
2025-12-15 03:08:48 +0300

    fix config gateway

--- infra/gateway-server/src/main/java/ru/practicum/Gateway.java
+++ infra/gateway-server/src/main/java/ru/practicum/Gateway.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class GatewayServer {
+public class Gateway {
     public static void main(String[] args) {
-        SpringApplication.run(GatewayServer.class, args);
+        SpringApplication.run(Gateway.class, args);
     }
 }
